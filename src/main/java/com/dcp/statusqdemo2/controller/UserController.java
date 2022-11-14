8a76f5a7de4ba2436f5e79ca2d7cc555bee2dcbf
dinindu99 <peiristd-im18057@stu.kln.ac.lk>
2022-11-14 08:58:46 +0530

    fixed structured json for login and sign up

--- src/main/java/com/dcp/statusqdemo2/controller/UserController.java
+++ src/main/java/com/dcp/statusqdemo2/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.dcp.statusqdemo2.controller;
 
+import com.dcp.statusqdemo2.dto.UserResponseDTO;
 import com.dcp.statusqdemo2.dto.UserDTO;
 import com.dcp.statusqdemo2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,46 @@ public class UserController {
     }
 
     @GetMapping("/getUser/{email}/{password}")
-    public UserDTO getUserByEmailAndPassword(@PathVariable("email") String email,@PathVariable("password") String password){
-        return userService.getUserByEmailAndPassword(email,password);
+    public UserResponseDTO getUserByEmailAndPassword(@PathVariable("email") String email, @PathVariable("password") String password){
+        UserResponseDTO userResponseDTO = new UserResponseDTO();
+
+        try{
+            UserDTO userDTO = userService.getUserByEmailAndPassword(email,password);
+            userResponseDTO.setUser(userDTO);
+            userResponseDTO.setResponseCode("00");
+            userResponseDTO.setStatus("success");
+            userResponseDTO.setMessage("You are logged successfully");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            userResponseDTO.setResponseCode("1000");
+            userResponseDTO.setStatus("failure");
+            userResponseDTO.setMessage("Invalid Username or Password");
+        }finally {
+            return userResponseDTO;
+        }
     }
 
     @PostMapping("/saveUser")
-    public UserDTO saveUser(@RequestBody UserDTO userDTO){
-        return userService.saveUser(userDTO);
+    public UserResponseDTO saveUser(@RequestBody UserDTO userDTO){
+        UserResponseDTO userResponseDTO = new UserResponseDTO();
+
+        try{
+            UserDTO checkUser = userService.getUserByEmail(userDTO.getEmail());
+            if(checkUser != null){
+                System.out.println(checkUser.getEmail());
+                userResponseDTO.setResponseCode("1000");
+                userResponseDTO.setStatus("error");
+                userResponseDTO.setMessage("Already you have an account");
+            }
+        }catch (Exception e){
+            userService.saveUser(userDTO);
+            userResponseDTO.setUser(userDTO);
+            userResponseDTO.setResponseCode("00");
+            userResponseDTO.setStatus("success");
+            userResponseDTO.setMessage("You are registered successfully");
+        }finally {
+            return userResponseDTO;
+        }
+
     }
 }
--- src/main/java/com/dcp/statusqdemo2/model/User.java
+++ src/main/java/com/dcp/statusqdemo2/model/User.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
@@ -21,6 +18,8 @@ public class User {
     private int userId;
     private String firstname;
     private String lastname;
+
+    @Column(unique = true)
     private String email;
     private String password;
     private String profileImageURL;
